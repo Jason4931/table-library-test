12374770dcad508165b98795fd7293455662c40f
ShadowLearner <edufirman159@gmail.com>
2025-06-26 13:24:55 +0700

    Add form example like repeater

---
+++ src/app/forms/page.jsx
@@ -0,0 +1,10 @@
+import MainLayout from "@/app/layout/mainlayout";
+import FormExample from "@/app/ui/forms/FormExample";
+
+export default function FormsPage() {
+  return (
+    <MainLayout>
+      <FormExample />
+    </MainLayout>
+  );
+}
---
+++ src/app/ui/forms/FormExample.jsx
@@ -0,0 +1,83 @@
+"use client";
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
+    var t = {};
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]];
+      }
+    return t;
+  };
+import React from "react";
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Space } from "antd";
+import "@ant-design/v5-patch-for-react-19";
+const onFinish = (values) => {
+  console.log("Received values of form:", values);
+};
+const FormExample = () => (
+  <Form
+    name="dynamic_form_nest_item"
+    onFinish={onFinish}
+    style={{ maxWidth: 600 }}
+    autoComplete="off"
+  >
+    <Form.List name="users">
+      {(fields, { add, remove }) => (
+        <>
+          {fields.map((_a) => {
+            var { key, name } = _a,
+              restField = __rest(_a, ["key", "name"]);
+            return (
+              <Space
+                key={key}
+                style={{ display: "flex", marginBottom: 8 }}
+                align="baseline"
+              >
+                <Form.Item
+                  {...restField}
+                  name={[name, "first"]}
+                  rules={[{ required: true, message: "Missing first name" }]}
+                >
+                  <Input placeholder="First Name" />
+                </Form.Item>
+                <Form.Item
+                  {...restField}
+                  name={[name, "last"]}
+                  rules={[{ required: true, message: "Missing last name" }]}
+                >
+                  <Input placeholder="Last Name" />
+                </Form.Item>
+                <MinusCircleOutlined onClick={() => remove(name)} />
+              </Space>
+            );
+          })}
+          <Form.Item>
+            <Button
+              type="dashed"
+              onClick={() => add()}
+              block
+              icon={<PlusOutlined />}
+            >
+              Add field
+            </Button>
+          </Form.Item>
+        </>
+      )}
+    </Form.List>
+    <Form.Item>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+);
+export default FormExample;
